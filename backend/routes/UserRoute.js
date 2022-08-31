57b94bb3840078346daf36650857d057ae9fc32a
Richard <richard.sinaga@ipm.co.id>
2022-08-31 15:22:40 +0700

    create user

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import {getUsers,getUsersById} from "../controller/UserController.js"
+import {getUsers,getUsersById, createUser} from "../controller/UserController.js"
 
 const router = express.Router();
 
 router.get('/users', getUsers)
 router.get('/users/:id', getUsersById)
+router.post('/users', createUser)
 
 export default router
